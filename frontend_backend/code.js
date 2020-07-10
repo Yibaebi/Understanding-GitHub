453f4ac40b030006f2987b850d4d66173eeb5920
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-07-10 13:18:49 +0100

    Update code.js

--- frontend_backend/code.js
+++ frontend_backend/code.js
@@ -6,11 +6,15 @@ alert ("Elliot Yibaebi (Frontend Development)");
 //Functions(using functions to determine the Area of a triangle and displaying the answers using alert box)
 b = 60, 30, 40;
 h = 50, 76, 80;
+//formular to determine the area of a triangle
 Area = 0.5*b*h;
+//function
 function triangle(b,h){
     Area = 0.5*b*h;
+    //alert box to display the answer
     alert('The Area ='+ Area);  
 }
+//calling the function
 triangle(60, 50);
 triangle(30, 76);
 triangle(40, 80);
