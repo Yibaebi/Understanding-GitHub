a410e248e638f76e476cb019d2a653129aaa73d5
Yibaebi <59321440+Yibaebi@users.noreply.github.com>
2020-07-10 18:26:35 +0100

    Update code.js

--- frontend_backend/code.js
+++ frontend_backend/code.js
@@ -1,6 +1,6 @@
 //Added by Elliot Yibaebi
 alert ("Elliot Yibaebi (Frontend Development)");
-alert("Ajaezo Kingsley (Front End Developer)");
+
 //Who will be next?
 
 // Francis Osinachi.
@@ -11,4 +11,7 @@ alert ("Chinedu Mbah (Backend Development)");
 
 //Added by Charles Ekube
 alert("Charles Ekube (Frontend Development)");
+
+
+alert("Ajaezo Kingsley (Front End Developer)");
 //Who will be next?
